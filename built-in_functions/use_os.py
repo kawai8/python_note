edf416cb7e25007e150a5873e4c89bcf92bc52ab
kawai8 <52528451+kawai8@users.noreply.github.com>
2021-07-29 20:18:20 +0900

    Create use_os.py

---
+++ built-in_functions/use_os.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os
+ 
+path = "/home/sho/lab/python"
+for root, dirs, files in os.walk(path):
+    for file in files:
+        print(file)
+
+#
+print(__file__)
+
+#
+print(os.getcwd())
+
+#
+print(os.path.basename(__file__))
+
+#
+print(os.path.abspath(__file__))
+
+#
+print(os.path.dirname(os.path.abspath(__file__)))
