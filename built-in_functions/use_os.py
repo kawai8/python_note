3dd47b163e8bc8a70f6c8029646595631e5a4455
kawai8 <52528451+kawai8@users.noreply.github.com>
2021-07-29 21:52:41 +0900

    Update use_os.py

--- built-in_functions/use_os.py
+++ built-in_functions/use_os.py
@@ -1,23 +1,23 @@
-#!/usr/bin/env python
-
 import os
- 
+
+##os.walk yields a 3-tuple (dirpath, dirnames, filenames) by walking the directory tree.
 path = "/foo/var/"
 for root, dirs, files in os.walk(path):
     for file in files:
         print(file)
 
-#
-print(__file__)
+#python 3.8 and earlier, '__file__' returns the path specified when executing.
+#python 3.9 and later, '__file__' always returns an absolute path.
+print('[__file__] = ' + __file__)
 
-#
-print(os.getcwd())
+#Return a string representing the current working directory.
+print('[os.getcwd] = ' + os.getcwd())
 
-#
-print(os.path.basename(__file__))
+#Return the base name of pathname path.
+print('[os.path.basename] = ' + os.path.basename(__file__))
 
-#
-print(os.path.abspath(__file__))
+#Return the directory name of pathname path.
+print('[os.path.dirname] = ' + os.path.dirname(__file__))
 
-#
-print(os.path.dirname(os.path.abspath(__file__)))
+#Return a normalized absolutized version of the pathname path.
+print('[os.path.abspath] = ' + os.path.abspath(__file__))
