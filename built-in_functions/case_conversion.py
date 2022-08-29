0e5aa5ec90906c214e2ac29ccce55d5665706010
kawai8 <52528451+kawai8@users.noreply.github.com>
2022-08-30 01:50:49 +0900

    Create case_conversion.py

---
+++ built-in_functions/case_conversion.py
@@ -0,0 +1,36 @@
+# class str
+# https://docs.python.org/3/library/stdtypes.html?highlight=upper#str.upper
+# https://docs.python.org/3/library/stdtypes.html?highlight=lower#str.lower
+# https://docs.python.org/3/library/stdtypes.html?highlight=lower#str.swapcase
+# https://docs.python.org/3/library/stdtypes.html?highlight=capitalize#str.capitalize
+# https://docs.python.org/3/library/stdtypes.html?highlight=capitalize#str.title
+
+
+# upper method
+>>> 'python'.upper()
+'PYTHON'
+>>> 'ｐｙｔｈｏｎ'.upper() # two-byte character
+'ＰＹＴＨＯＮ'
+
+
+# lower method
+>>> 'PYTHON'.lower()
+'python'
+>>> 'ＰＹＴＨＯＮ'.lower() # two-byte character
+'ｐｙｔｈｏｎ'
+
+
+# swapcase
+>>> 'Hello Python'.swapcase()
+'hELLO pYTHON'
+
+
+# capitalize
+>>> 'python programmer'.capitalize()
+'Python programmer'
+
+
+# title
+>>> 'python programmer'.title()
+'Python Programmer'
+
