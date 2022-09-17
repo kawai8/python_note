b7fed018ffcb9d6531cdfb281f2b2615871dbac4
kawai8 <52528451+kawai8@users.noreply.github.com>
2022-09-18 01:38:33 +0900

    Update indexing.py

--- indexing.py
+++ indexing.py
@@ -8,3 +8,12 @@ print(str1[0])
 print(str1[-1]) # last character
 print(str1[len(str1) -1]) 
 # n
+
+
+
+# Attempting to use an index that is too large will result in an error
+print(str1[10])
+# Traceback (most recent call last):
+#   File "006.py", line 33, in <module>
+#     print(str1[10])
+# IndexError: string index out of range
