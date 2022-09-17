01f3967dd7618a22acf2dd3353eddb3a5fe95605
kawai8 <52528451+kawai8@users.noreply.github.com>
2022-09-18 01:39:25 +0900

    Create slicing.py

---
+++ slicing.py
@@ -0,0 +1,35 @@
+# Slicing
+'''
+ +---+---+---+---+---+---+
+ | P | y | t | h | o | n |
+ +---+---+---+---+---+---+
+ 0   1   2   3   4   5   6
+-6  -5  -4  -3  -2  -1
+'''
+
+
+
+str1 = 'python'
+print(str1[2:5])
+# tho
+
+# an omitted first index defaults to zero
+print(str1[:2])
+# py
+
+# an omitted second index defaults to the size of the string being sliced.
+print(str1[4:])
+print(str1[-2:])
+# on
+
+
+# Note how the start is always included, and the end always excluded.
+print(str1[:2] + str1[2:])
+# python
+
+
+
+# out of range slice indexes are handled gracefully when used for slicing:
+print(str1[0:10])
+# python
+
