02bf43800f06c78f290c7eaeb794028854973dc8
kawai8 <52528451+kawai8@users.noreply.github.com>
2022-09-18 01:23:34 +0900

    Create indexing.py

---
+++ indexing.py
@@ -0,0 +1,10 @@
+# Indexing
+str1 = 'python'
+print(str1[0])
+# p
+
+
+# Note that since -0 is the same as 0, negative indices start from -1.
+print(str1[-1]) # last character
+print(str1[len(str1) -1]) 
+# n
