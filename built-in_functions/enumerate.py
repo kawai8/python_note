4e0c51f76b619d87811d60c0bb6ea927ea5ed50a
kawai8 <52528451+kawai8@users.noreply.github.com>
2023-06-07 03:44:52 +0900

    Update enumerate.py

--- built-in_functions/enumerate.py
+++ built-in_functions/enumerate.py
@@ -1,7 +1,15 @@
 # enumerate(iterable, start=0)
+
 my_list = ['cat', 'dog', 'bird']
 for i, animal in enumerate(my_list, start=1)
     print(i, animal)
 # 1 cat
 # 2 dog
 # 3 bird
+
+
+seasons = ['Spring', 'Summer', 'Fall', 'Winter']
+list(enumerate(seasons))
+# [(0, 'Spring'), (1, 'Summer'), (2, 'Fall'), (3, 'Winter')]
+
+
