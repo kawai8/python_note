48c87a1aa220151779edce38dd22befbc85486bc
kawai8 <52528451+kawai8@users.noreply.github.com>
2023-06-12 00:45:31 +0900

    Update string_methods.py

--- string_methods.py
+++ string_methods.py
@@ -2,287 +2,290 @@
 # https://docs.python.org/3/library/stdtypes.html#string-methods
 
 
-# capitalize method
-# str.capitalize()
+##  capitalize method
+##  str.capitalize()
 'python programmer'.capitalize()
 # 'Python programmer'
 
 
-# casefole method
-# str.casefold()
-example
-# 
-
-# cetner method
-# str.center(width[, fillchar])
+## casefole method
+## str.casefold()
 example
 # 
 
 
-# count method
-# str.count(sub[, start[, end]])
+## cetner method
+## str.center(width[, fillchar])
+example
+# 
+
+
+## count method
+## str.count(sub[, start[, end]])
 'cat cat dog cat dog'.count('cat')
 # 3
 
 
-# encode method
-# str.encode(encoding='utf-8', errors='strict')
-example
-# output
-
-# endswith method
-# str.endswith(suffix[, start[, end]])
+## encode method
+## str.encode(encoding='utf-8', errors='strict')
 example
 # output
 
 
-# expandtabs method
-# str.expandtabs(tabsize=8)
+## endswith method
+## str.endswith(suffix[, start[, end]])
 example
 # output
 
 
-# find method
-# str.find(sub[, start[, end]])
+## expandtabs method
+## str.expandtabs(tabsize=8)
+example
+# output
+
+
+## find method
+## str.find(sub[, start[, end]])
 'abcdabcdabcd'.find('a', 2, 8)
 # 4
 
 
-# format method
-# str.format(*args, **kwargs)
+## format method
+## str.format(*args, **kwargs)
 example
 # output
 
 
-# format_map method
-#str.format_map(mapping)
+## format_map method
+## str.format_map(mapping)
 example
 # output
 
 
-# index method
-# str.index(sub[, start[, end]])
+## index method
+## str.index(sub[, start[, end]])
 'abcdabcdabcd'.index('a', 2, 8)
 # 4
 
 
-# isalnum method
-# str.isalnum()
+## isalnum method
+## str.isalnum()
 example
 # output
 
 
-# isalpha method
-# str.isalpha()
+## isalpha method
+## str.isalpha()
 example
 # output
 
 
-# isascii method
-# str.isascii()
+## isascii method
+## str.isascii()
 example
 # output
 
 
-# isdecimal method
-# str.isdecimal()
+## isdecimal method
+## str.isdecimal()
 example
 # output
 
 
-# isdigit method
-# str.isdigit()
+## isdigit method
+## str.isdigit()
 example
 # output
 
 
-# isidentifier method
-# str.isidentifier()
+## isidentifier method
+## str.isidentifier()
 example
 # output
 
 
-# islower method
-# str.islower()
+## islower method
+## str.islower()
 example
 # output
 
 
-# isnumeric method
-# str.isnumeric()
+## isnumeric method
+## str.isnumeric()
 example
 # output
 
 
-# isprintable method
-# str.isprintable()
+## isprintable method
+## str.isprintable()
 example
 # output
 
 
-# isspace method
-# str.isspace()
+## isspace method
+## str.isspace()
 example
 # output
 
 
-# istitle method
-# str.istitle()
+## istitle method
+## str.istitle()
 example
 # output
 
 
-# isupper method
-# str.isupper()
+## isupper method
+## str.isupper()
 example
 # output
 
 
-# join method
-#str.join(iterable)
+## join method
+##str.join(iterable)
 example
 # output
 
 
-# ljust method
-# str.ljust(width[, fillchar])
+## ljust method
+## str.ljust(width[, fillchar])
 example
 # output
 
 
-# lower method
-# str.lower()
+## lower method
+## str.lower()
 'PYTHON'.lower()
 # 'python'
 'ＰＹＴＨＯＮ'.lower() # two-byte character
 # 'ｐｙｔｈｏｎ'
 
 
-# lstrip method
-# str.lstrip([chars])
+## lstrip method
+## str.lstrip([chars])
 example
 # output
 
 
-# maketrans method
-# static str.maketrans(x[, y[, z]])
+## maketrans method
+## static str.maketrans(x[, y[, z]])
 example
 # output
 
 
-# partition method
-# str.partition(sep)
+## partition method
+## str.partition(sep)
 example
 # output
 
 
-# removeprefix method
-# str.removeprefix(prefix, /)
+## removeprefix method
+## str.removeprefix(prefix, /)
 example
 # output
 
 
-# removesuffix method
-# str.removesuffix(suffix, /)
+## removesuffix method
+## str.removesuffix(suffix, /)
 example
 # output
 
 
-# replace method
-# str.replace(old, new[, count])
+## replace method
+## str.replace(old, new[, count])
 'python programmer'.replace("python", "C")
 # 'C programmer'
 'foo foo foo bar foo bar bar'.replace("foo", "run", 2)
 # 'run run foo bar foo bar bar'
 
 
-# rfind method
-# str.rfind(sub[, start[, end]])
+## rfind method
+## str.rfind(sub[, start[, end]])
 'abcdabcdabcd'.rfind('a', 2, 10)
 # 8
 
 
-# rindex method
-# str.rindex(sub[, start[, end]])
+## rindex method
+## str.rindex(sub[, start[, end]])
 example
 # output
 
 
-# rjust method
-# str.rjust(width[, fillchar])
+## rjust method
+## str.rjust(width[, fillchar])
 example
 # output
 
 
-# rpartition method
-# str.rpartition(sep)
+## rpartition method
+## str.rpartition(sep)
 example
 # output
 
 
-# rsplit method
-# str.rsplit(sep=None, maxsplit=- 1)
+## rsplit method
+## str.rsplit(sep=None, maxsplit=- 1)
 example
 # output
 
 
-# rstrip method
-# str.rstrip([chars])
+## rstrip method
+## str.rstrip([chars])
 example
 # output
 
 
-# split method
-# str.split(sep=None, maxsplit=- 1)
+## split method
+## str.split(sep=None, maxsplit=- 1)
 example
 # output
 
 
-# splitlines method
-# str.splitlines(keepends=False)
+## splitlines method
+## str.splitlines(keepends=False)
 example
 # output
 
 
-# startswith method
-# str.startswith(prefix[, start[, end]])
+## startswith method
+## str.startswith(prefix[, start[, end]])
 example
 # output
 
 
-# strip method
-# str.strip([chars])
+## strip method
+## str.strip([chars])
 example
 # output
 
 
-# swapcase method
-# str.swapcase()
+## swapcase method
+## str.swapcase()
 'Hello Python'.swapcase()
 # 'hELLO pYTHON'
 
 
-# title method
-# str.title()
+## title method
+## str.title()
 'python programmer'.title()
 # 'Python Programmer'
 
 
-# translate method
-# str.translate(table)
+## translate method
+## str.translate(table)
 'engin engin engin'.translate(str.maketrans("eng", "ENG"))
 #'ENGiN ENGiN ENGiN'
 
 
-# upper method
+## upper method
+## str.upper()
 'python'.upper()
 # 'PYTHON'
 'ｐｙｔｈｏｎ'.upper() # two-byte character
 # 'ＰＹＴＨＯＮ'
 
 
-# zfill method
-# str.zfill(width)
+## zfill method
+## str.zfill(width)
 example
 # output
 
