9aeefdfff31ae8babd8ce0b90141fba6caa44564
kawai8 <52528451+kawai8@users.noreply.github.com>
2023-06-12 00:28:16 +0900

    Update string_methods.py

--- string_methods.py
+++ string_methods.py
@@ -2,33 +2,190 @@
 # https://docs.python.org/3/library/stdtypes.html#string-methods
 
 
-## upper method
-'python'.upper()
-# 'PYTHON'
-'ｐｙｔｈｏｎ'.upper() # two-byte character
-# 'ＰＹＴＨＯＮ'
+# capitalize method
+# str.capitalize()
+'python programmer'.capitalize()
+# 'Python programmer'
+
+
+# casefole method
+# str.casefold()
+example
+# 
+
+# cetner method
+# str.center(width[, fillchar])
+example
+# 
+
+
+# count method
+# str.count(sub[, start[, end]])
+'cat cat dog cat dog'.count('cat')
+# 3
+
+
+# encode method
+# str.encode(encoding='utf-8', errors='strict')
+example
+# output
+
+# endswith method
+# str.endswith(suffix[, start[, end]])
+example
+# output
+
+
+# expandtabs method
+# str.expandtabs(tabsize=8)
+example
+# output
+
+
+# find method
+# str.find(sub[, start[, end]])
+'abcdabcdabcd'.find('a', 2, 8)
+# 4
+
+
+# format method
+# str.format(*args, **kwargs)
+example
+# output
+
+
+# format_map method
+#str.format_map(mapping)
+example
+# output
+
+
+# index method
+# str.index(sub[, start[, end]])
+'abcdabcdabcd'.index('a', 2, 8)
+# 4
+
+
+# isalnum method
+# str.isalnum()
+example
+# output
+
+
+# isalpha method
+# str.isalpha()
+example
+# output
+
+
+# isascii method
+# str.isascii()
+example
+# output
+
+
+# isdecimal method
+# str.isdecimal()
+example
+# output
+
+
+# isdigit method
+# str.isdigit()
+example
+# output
+
+
+# isidentifier method
+# str.isidentifier()
+example
+# output
+
+
+# islower method
+# str.islower()
+example
+# output
+
+
+# isnumeric method
+# str.isnumeric()
+example
+# output
+
+
+# isprintable method
+# str.isprintable()
+example
+# output
+
+
+# isspace method
+# str.isspace()
+example
+# output
+
+
+# istitle method
+# str.istitle()
+example
+# output
+
+
+# isupper method
+# str.isupper()
+example
+# output
+
+
+# join method
+#str.join(iterable)
+example
+# output
+
+
+# ljust method
+# str.ljust(width[, fillchar])
+example
+# output
 
 
 # lower method
+# str.lower()
 'PYTHON'.lower()
 # 'python'
 'ＰＹＴＨＯＮ'.lower() # two-byte character
 # 'ｐｙｔｈｏｎ'
 
 
-# swapcase method
-'Hello Python'.swapcase()
-# 'hELLO pYTHON'
+# lstrip method
+# str.lstrip([chars])
+example
+# output
 
 
-# capitalize method
-'python programmer'.capitalize()
-# 'Python programmer'
+# maketrans method
+# static str.maketrans(x[, y[, z]])
+example
+# output
 
 
-# title method
-'python programmer'.title()
-# 'Python Programmer'
+# partition method
+# str.partition(sep)
+example
+# output
+
+
+# removeprefix method
+# str.removeprefix(prefix, /)
+example
+# output
+
+
+# removesuffix method
+# str.removesuffix(suffix, /)
+example
+# output
 
 
 # replace method
@@ -39,31 +196,93 @@
 # 'run run foo bar foo bar bar'
 
 
+# rfind method
+# str.rfind(sub[, start[, end]])
+'abcdabcdabcd'.rfind('a', 2, 10)
+# 8
+
+
+# rindex method
+# str.rindex(sub[, start[, end]])
+example
+# output
+
+
+# rjust method
+# str.rjust(width[, fillchar])
+example
+# output
+
+
+# rpartition method
+# str.rpartition(sep)
+example
+# output
+
+
+# rsplit method
+# str.rsplit(sep=None, maxsplit=- 1)
+example
+# output
+
+
+# rstrip method
+# str.rstrip([chars])
+example
+# output
+
+
+# split method
+# str.split(sep=None, maxsplit=- 1)
+example
+# output
+
+
+# splitlines method
+# str.splitlines(keepends=False)
+example
+# output
+
+
+# startswith method
+# str.startswith(prefix[, start[, end]])
+example
+# output
+
+
+# strip method
+# str.strip([chars])
+example
+# output
+
+
+# swapcase method
+# str.swapcase()
+'Hello Python'.swapcase()
+# 'hELLO pYTHON'
+
+
+# title method
+# str.title()
+'python programmer'.title()
+# 'Python Programmer'
+
+
 # translate method
 # str.translate(table)
 'engin engin engin'.translate(str.maketrans("eng", "ENG"))
 #'ENGiN ENGiN ENGiN'
 
 
-# find method
-# str.find(sub[, start[, end]])
-'abcdabcdabcd'.find('a', 2, 8)
-# 4
+# upper method
+'python'.upper()
+# 'PYTHON'
+'ｐｙｔｈｏｎ'.upper() # two-byte character
+# 'ＰＹＴＨＯＮ'
 
 
-# rfind method
-# str.rfind(sub[, start[, end]])
-'abcdabcdabcd'.rfind('a', 2, 10)
-# 8
-
-# index method
-# str.index(sub[, start[, end]])
-'abcdabcdabcd'.index('a', 2, 8)
-# 4
-
-
-# count method
-# str.count(sub[, start[, end]])
-'cat cat dog cat dog'.count('cat')
-# 3
+# zfill method
+# str.zfill(width)
+example
+# output
 
