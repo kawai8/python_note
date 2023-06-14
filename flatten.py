0d3af4cab45a349e11f0a60a5ddce4ff749c324e
kawai8 <52528451+kawai8@users.noreply.github.com>
2023-06-15 01:56:38 +0900

    Update flatten.py

--- flatten.py
+++ flatten.py
@@ -4,3 +4,11 @@ flattened = [x for tup in some_tuples for x in tup]
 
 print(flattened)
 # [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
+#------------------------------------------
+flattened = []
+for tup in some_tuples:
+    for x in tup:
+        flattened.append(x)
