453e61343303a74630f5ae9baa4edddc07fd852f
kawai8 <52528451+kawai8@users.noreply.github.com>
2024-09-24 04:03:22 +0900

    Update capitalize_method.py

--- capitalize_method.py
+++ capitalize_method.py
@@ -11,10 +11,11 @@ none
 
 Return
 a copy of the string with its first character capitalized and the rest lowercased
-
 """
 
+# ------------------------------------------
 # Example
+# ------------------------------------------
 print('python programmer'.capitalize())
 # 'Python programmer'
-~                                                                                                  
+                                                                                               
