57bb975cbc6c97e020a6c920c4cf036d0dea3e23
kawai8 <52528451+kawai8@users.noreply.github.com>
2024-09-24 04:00:32 +0900

    Create capitalize_method.py

---
+++ capitalize_method.py
@@ -0,0 +1,20 @@
+# capitalize method
+# class str
+# https://docs.python.org/3/library/stdtypes.html#str.capitalize
+
+"""
+Syntax
+str.capitalize()
+
+Parameter
+none 
+
+Return
+a copy of the string with its first character capitalized and the rest lowercased
+
+"""
+
+# Example
+print('python programmer'.capitalize())
+# 'Python programmer'
+~                                                                                                  
