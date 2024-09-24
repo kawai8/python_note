6b26f9f2160e2fe340aecce888352288d032241c
kawai8 <52528451+kawai8@users.noreply.github.com>
2024-09-25 04:37:58 +0900

    Create find_method.py

---
+++ find_method.py
@@ -0,0 +1,22 @@
+# find method
+# str class
+# https://docs.python.org/3/library/stdtypes.html#str.find
+
+"""
+[Syntax]
+str.find(sub[, start[, end]])
+
+[Parameter]
+sub: substring
+start: starting index
+end: ending index
+
+[Return]
+the lowest index in the string where substring sub is found within the slice s[start:end].
+"""
+
+# ------------------------------------------
+# Example
+# ------------------------------------------
+print('abcdabcdabcd'.find('a', 2, 8))
+# output: 4
