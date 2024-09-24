927b2104b8c13172aaca279b7c45f2ebae046163
kawai8 <52528451+kawai8@users.noreply.github.com>
2024-09-25 03:43:42 +0900

    Create endswith_method.py

---
+++ endswith_method.py
@@ -0,0 +1,26 @@
+# endswith method
+# str class
+# https://docs.python.org/3/library/stdtypes.html#str.endswith
+
+"""
+[Syntax]
+str.endswith(suffix[, start[, end]])
+
+[Parameter]
+suffix: string or tuple of suffixes
+start: beginning position
+end: ending position
+
+[Return]
+True if the string ends with the specified suffix.
+
+"""
+
+# ------------------------------------------
+# Example
+# ------------------------------------------
+print('python programmer'.endswith('mmer'))
+# output: True  
+
+print('suffix can also be a tuple of suffixes to look for.'.endswith(('for.', 'can')))
+# output: True  
