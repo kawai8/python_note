ecb65cbe1735751184780039577c258787dc4e4b
kawai8 <52528451+kawai8@users.noreply.github.com>
2024-09-25 02:09:04 +0900

    Create center_method.py

---
+++ center_method.py
@@ -0,0 +1,22 @@
+# center method
+# str class
+# https://docs.python.org/3/library/stdtypes.html#str.center
+
+"""
+[Syntax]
+str.center(width[, fillchar])
+
+[Parameter]
+width: length of the string
+fillchar: padding character
+
+[Return]
+a centered string of length width padded with specified fillchar
+"""
+
+# ------------------------------------------
+# Example
+# ------------------------------------------
+print('apple'.center(8, '_'))
+# output: _apple__
+                                                                               
