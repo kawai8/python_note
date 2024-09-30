38e2c545fbb5817f54a21d193eb9c4e4df0016eb
kawai8 <52528451+kawai8@users.noreply.github.com>
2024-10-01 01:17:19 +0900

    Create ljust_method.py

---
+++ ljust_method.py
@@ -0,0 +1,21 @@
+# ljust method
+# str class
+# https://docs.python.org/3/library/stdtypes.html#str.ljust
+
+"""
+[Syntax]
+str.ljust(width[, fillchar])
+
+[Parameter]
+width: width of the returned string
+fillchar: padding character
+
+[Return]
+a lefted string of length width padded with specified fillchar
+"""
+
+# ------------------------------------------
+# Example
+# ------------------------------------------
+print('apple'.ljust(8, '_'))
+# output: apple___
