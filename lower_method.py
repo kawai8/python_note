413dcf6298be2dea16f5860481e9841f8eee1e56
kawai8 <52528451+kawai8@users.noreply.github.com>
2024-10-01 01:28:49 +0900

    Create lower_method.py

---
+++ lower_method.py
@@ -0,0 +1,23 @@
+# lower method
+# str class
+# https://docs.python.org/3/library/stdtypes.html#str.lower
+
+"""
+[Syntax]
+str.lower()
+
+[Parameter]
+none 
+
+[Return]
+a copy of the string with all the cased characters converted to lowercase.
+"""
+
+# ------------------------------------------
+# Example
+# ------------------------------------------
+print('PYTHON'.lower())
+# output: python
+
+print('ＰＹＴＨＯＮ'.lower())
+# output: ｐｙｔｈｏｎ
