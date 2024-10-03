14feb603b6c72958f878135a23c82918b6c51b72
kawai8 <52528451+kawai8@users.noreply.github.com>
2024-10-04 03:23:24 +0900

    Create partition_method.py

---
+++ partition_method.py
@@ -0,0 +1,20 @@
+# partition method
+# str class
+# https://docs.python.org/3/library/stdtypes.html#str.partition
+
+"""
+[Syntax]
+str.partition(sep)
+
+[Parameter]
+sep: separator 
+
+[Return]
+a 3-tuple containing the part before the separator, the separator itself, and the part after the separator. 
+"""
+
+# ------------------------------------------
+# Example
+# ------------------------------------------
+print('example@xxx.come'.partition('@'))
+# output: ('example', '@', 'xxx.come')
