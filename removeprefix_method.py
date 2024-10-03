131528500cea22c4719d8d2057242554b2229c3e
kawai8 <52528451+kawai8@users.noreply.github.com>
2024-10-04 03:50:48 +0900

    Create removeprefix_method.py

---
+++ removeprefix_method.py
@@ -0,0 +1,25 @@
+# removeprefix method
+# str class
+# https://docs.python.org/3/library/stdtypes.html#str.removeprefix
+# Added in version 3.9.
+
+"""
+[Syntax]
+str.removeprefix(prefix, /)
+
+[Parameter]
+prefix: prefix string
+
+[Return]
+string[len(prefix):].
+"""
+
+# ------------------------------------------
+# Example
+# ------------------------------------------
+print('TestHook'.removeprefix('Test'))
+print('TestHook'[len('Test'):])
+# output: Hook
+
+print('BaseTestHook'.removeprefix('Test'))
+# output: BaseTestHook
