e09d679f907987d9bbebfa3812666cc260c6fb0c
kawai8 <52528451+kawai8@users.noreply.github.com>
2024-10-06 04:02:00 +0900

    Create rfind_method.py

---
+++ rfind_method.py
@@ -0,0 +1,28 @@
+# rfind method
+# str class
+# https://docs.python.org/3/library/stdtypes.html#str.rfind
+
+"""
+[Syntax]
+str.rfind(sub[, start[, end]])
+
+[Parameter]
+sub: the substring to search for 
+start: start index
+end: end index
+
+[Return]
+the highest index in the string where substring sub is found, such that sub is contained within s[start:end]. 
+"""
+
+# ------------------------------------------
+# Example
+# ------------------------------------------
+print('0123456789'.rfind('3'))
+# output: 3
+
+print('01234567893'.rfind('3'))
+# output : 10
+
+print('01234567893'.rfind('3', 1, 10))
+# output : 3
