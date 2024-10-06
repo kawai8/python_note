ac17d2c2523c8925987b75d549f18731e88020a7
kawai8 <52528451+kawai8@users.noreply.github.com>
2024-10-07 00:38:36 +0900

    Create rindex_method.py

---
+++ rindex_method.py
@@ -0,0 +1,31 @@
+# rindex method
+# str class
+# https://docs.python.org/3/library/stdtypes.html#str.rindex
+
+"""
+[Syntax]
+str.rindex(sub[, start[, end]])
+
+[Parameter]
+sub: the substring to search for
+start: start index
+end: end index
+
+[Return]
+the highest index in the string where substring sub is found, such that sub is contained within s[start:end]. 
+"""
+
+# ------------------------------------------
+# Example
+# ------------------------------------------
+print('0123456789'.rindex('3'))
+# output: 3
+
+print('01234567893'.rindex('3'))
+# output : 10
+
+print('01234567893'.rindex('3', 1, 10))
+# output : 3
+
+#print('01234567893'.rindex('a', 1, 10))
+# output : ValueError: substring not found
