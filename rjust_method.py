847de54a9fa69accea1f53388c71b754ffc430d7
kawai8 <52528451+kawai8@users.noreply.github.com>
2024-10-07 00:52:50 +0900

    Create rjust_method.py

---
+++ rjust_method.py
@@ -0,0 +1,21 @@
+# rjust method
+# str class
+# https://docs.python.org/3/library/stdtypes.html#str.rjust
+
+"""
+[Syntax]
+str.rjust(width[, fillchar])
+
+[Parameter]
+width: the width of the returned string 
+filchar: padding character
+
+[Return]
+the string right justified in a string of length width.
+"""
+
+# ------------------------------------------
+# Example
+# ------------------------------------------
+print('programmer'.rjust(20, '*'))
+# output: **********programmer
