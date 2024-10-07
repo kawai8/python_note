0af128e4daae1e964475eb7d3af0a67297cff520
kawai8 <52528451+kawai8@users.noreply.github.com>
2024-10-08 01:59:24 +0900

    Create startswith_method.py

---
+++ startswith_method.py
@@ -0,0 +1,22 @@
+# startswith method
+# str class
+# https://docs.python.org/3/library/stdtypes.html#str.startswith
+
+"""
+[Syntax]
+str.startswith(prefix[, start[, end]])
+
+[Parameter]
+prefix: string or tuple
+start: start position
+end: end position
+
+[Return]
+True if string starts with the prefix
+"""
+
+# ------------------------------------------
+# Example
+# ------------------------------------------
+print('python programmer'.startswith('py'))
+# output: True
