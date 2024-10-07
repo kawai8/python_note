f8f95c812ec6ebd2f99cb6640c38d31b21950991
kawai8 <52528451+kawai8@users.noreply.github.com>
2024-10-08 02:16:54 +0900

    Create strip_method.py

---
+++ strip_method.py
@@ -0,0 +1,23 @@
+# strip method
+# str class
+# https://docs.python.org/3/library/stdtypes.html#str.strip
+
+"""
+[Syntax]
+str.strip(chars])
+
+[Parameter]
+chars: a string specifying the set of characters to be removed. 
+
+[Return]
+a copy of the string with the leading and trailing characters removed
+"""
+
+# ------------------------------------------
+# Example
+# ------------------------------------------
+print("'" + '  spacious   '.strip() + "'")
+# output: 'spacious   '
+
+print("'" + 'www.example.com'.strip('cmowz.') + "'")
+# output: 'example.com'
