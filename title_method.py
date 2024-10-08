4857959c2a379154aa9bc692113a2d5be667fd95
kawai8 <52528451+kawai8@users.noreply.github.com>
2024-10-09 03:13:14 +0900

    Update title_method.py

--- title_method.py
+++ title_method.py
@@ -18,3 +18,6 @@ a titlecased version of the string where words start with an uppercase character
 # ------------------------------------------
 print('python programmer'.title())
 # output: Python Programmer
+
+print('漢字aaa'.title())
+# output: 漢字Aaa
