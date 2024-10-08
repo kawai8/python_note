b2a288cefb675a427c82849e9ee4a5a2491a94f0
kawai8 <52528451+kawai8@users.noreply.github.com>
2024-10-09 03:00:52 +0900

    Create swapcase_method.py

---
+++ swapcase_method.py
@@ -0,0 +1,19 @@
+# swapcase method
+# str class
+# https://docs.python.org/3/library/stdtypes.html#str.swapcase
+
+"""
+[Syntax]
+str.swapcase()
+
+[Parameter]
+none 
+
+[Return]
+a copy of the string with uppercase characters converted to lowercase and vice versa"""
+
+# ------------------------------------------
+# Example
+# ------------------------------------------
+print('Python Programmer'.swapcase())
+# output: pYTHON pROGRAMMER
