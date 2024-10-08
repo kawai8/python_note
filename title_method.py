ef2a10db3b27ec18d33d4c06a686caa204309d09
kawai8 <52528451+kawai8@users.noreply.github.com>
2024-10-09 03:07:14 +0900

    Create title_method.py

---
+++ title_method.py
@@ -0,0 +1,20 @@
+# title method
+# str class
+# https://docs.python.org/3/library/stdtypes.html#str.title
+
+"""
+[Syntax]
+str.title()
+
+[Parameter]
+none 
+
+[Return]
+a titlecased version of the string where words start with an uppercase character and the remaining characters are lowercase
+"""
+
+# ------------------------------------------
+# Example
+# ------------------------------------------
+print('python programmer'.title())
+# output: Python Programmer
