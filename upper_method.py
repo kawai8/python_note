6cf142979e0f96d26a2857e0f9c965419c9b76b3
kawai8 <52528451+kawai8@users.noreply.github.com>
2024-10-10 02:17:20 +0900

    Create upper_method.py

---
+++ upper_method.py
@@ -0,0 +1,20 @@
+# upper method
+# str class
+# https://docs.python.org/3/library/stdtypes.html#str.upper
+
+"""
+[Syntax]
+str.upper()
+
+[Parameter]
+none 
+
+[Return]
+a copy of the string with all the cased characters converted to uppercase
+"""
+
+# ------------------------------------------
+# Example
+# ------------------------------------------
+print('python programmer'.upper())
+# output: PYTHON PROGRAMMER
